bba7295f02d25f338508ee11f5cfce09f9c19b70
Luke Edwards <luke.edwards05@gmail.com>
2017-07-28 21:26:47 -0700

    [refactor] split classes into own files; use arr/*

---
+++ src/node.js
@@ -0,0 +1,87 @@
+import find from '@arr/find';
+import filter from '@arr/filter';
+import { findWhere, toLower, splice } from './util';
+
+const getChild = (el, fn, isMany) => (isMany ? filter : find)(el.childNodes, fn) || null;
+
+export default class Node {
+	constructor(type, name) {
+		this.nodeType = type;
+		this.nodeName = name;
+		this.childNodes = [];
+	}
+
+	get nextSibling() {
+		let p = this.parentNode;
+		if (p) return p.childNodes[findWhere(p.childNodes, this, true) + 1];
+	}
+
+	get previousSibling() {
+		let p = this.parentNode;
+		if (p) return p.childNodes[findWhere(p.childNodes, this, true) - 1];
+	}
+
+	get firstChild() {
+		return this.childNodes[0];
+	}
+
+	get lastChild() {
+		return this.childNodes[this.childNodes.length - 1];
+	}
+
+	getElementById(str) {
+		return getChild(this, o => (o.getAttribute('id') === str) && o);
+	}
+
+	getElementsByClassName(str) {
+		return getChild(this, o => ~(o.className || '').indexOf(str), true);
+	}
+
+	querySelector(str) {
+		console.log('qs got', str);
+		var isID = /#(.*)/.exec(str);
+		if (isID) return this.getElementById(isID[1]);
+		var isCls = /(.*)\.(.*)/.exec(str);
+		if (isCls) {
+			var el = isCls[1];
+			var cls = isCls[2];
+			if (el) {
+				var th = this.childNodes.filter(o => toLower(o.nodeName) === el);
+				if (cls) {
+					return filter(th)
+					return th.filter(o => ~(o.className || '').indexOf(cls))[0];
+				} else {
+					return th[0];
+				}
+			}
+		}
+		return find(this.childNodes, o => toLower(o.nodeName)===str);
+	}
+
+	appendChild(child) {
+		this.insertBefore(child);
+	}
+
+	insertBefore(child, ref) {
+		child.remove();
+		child.parentNode = this;
+		ref ? splice(this.childNodes, ref, child) : this.childNodes.push(child);
+	}
+
+	replaceChild(child, ref) {
+		if (ref.parentNode === this) {
+			this.insertBefore(child, ref);
+			ref.remove();
+		}
+	}
+
+	removeChild(child) {
+		splice(this.childNodes, child);
+	}
+
+	remove() {
+		if (this.parentNode) {
+			this.parentNode.removeChild(this);
+		}
+	}
+}
--- src/util.js
+++ src/util.js
@@ -1,9 +1,18 @@
-export function assign(obj, props) {
-	for (let i in props) obj[i] = props[i];
-}
+/*
+const NODE_TYPES = {
+	ELEMENT_NODE: 1,
+	ATTRIBUTE_NODE: 2,
+	TEXT_NODE: 3,
+	CDATA_SECTION_NODE: 4,
+	ENTITY_REFERENCE_NODE: 5,
+	COMMENT_NODE: 6,
+	PROCESSING_INSTRUCTION_NODE: 7,
+	DOCUMENT_NODE: 9
+};
+*/
 
 export function toLower(str) {
-	return String(str).toLowerCase();
+	return str.toLowerCase();
 }
 
 export function splice(arr, item, add, byValueOnly) {
